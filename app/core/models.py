bc6a5b3fca523d01dd60fb93eaefba3e8357df0a
maximegagne <max.8989@hotmail.com>
2020-08-10 13:32:20 -0400

    Added working list tag endpoint

--- app/core/models.py
+++ app/core/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, \
     BaseUserManager, PermissionsMixin
+from django.conf import settings
 
 
 # We override the create users function
@@ -36,3 +37,15 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
+
+
+class Tag(models.Model):
+    """Tag to be use for recipe"""
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.name
